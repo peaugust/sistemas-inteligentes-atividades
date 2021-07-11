5ed6cf49d9573970e8849a88d3cb3b5b5e90a634
Pedro Freddi <pedro.freddi@jungledevs.com>
2021-07-11 14:47:25 -0300

    feat: Add isSolvable

--- index.js
+++ index.js
@@ -1,41 +1,11 @@
 import { expandNode, createNode } from './Node.js'
-
-// Adding array comparison
-// Warn if overriding existing method
-if (Array.prototype.equals)
-  console.warn(
-    "Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code."
-  )
-// attach the .equals method to Array's prototype to call it on any array
-Array.prototype.equals = function (array) {
-  // if the other array is a falsy value, return
-  if (!array) return false
-
-  // compare lengths - can save a lot of time
-  if (this.length != array.length) return false
-
-  for (var i = 0, l = this.length; i < l; i++) {
-    // Check if we have nested arrays
-    if (this[i] instanceof Array && array[i] instanceof Array) {
-      // recurse into the nested arrays
-      if (!this[i].equals(array[i])) return false
-    } else if (this[i] != array[i]) {
-      // Warning - two different object instances will never be equal: {x:20} != {x:20}
-      return false
-    }
-  }
-  return true
-}
-// Hide method from for-in loops
-Object.defineProperty(Array.prototype, 'equals', { enumerable: false })
-
-const goalState = [1, 2, 3, 4, 5, 6, 7, 8, ' ']
-
-const displayBoard = (state) => {
-  console.log(`| ${state[0]} | ${state[1]} | ${state[2]} |\n| ${state[3]} | ${state[4]} | ${state[5]} |\n| ${state[6]} | ${state[7]} | ${state[8]} |\n`)
-}
+import { goalState, displayBoard, generateNewBoard } from './Board.js'
+import Array from './Array.js'
 
 const uniformCost = (startState, goalState) => {
+  console.log('----------- Running Uniform Cost -----------')
+
+  console.time()
   const startNode = createNode(startState, null, null, 0, 0)
   let fringe = []
   let visited = []
@@ -58,7 +28,7 @@ const uniformCost = (startState, goalState) => {
     visited.push(current)
     current = fringe.shift()
   }
-
+  console.timeEnd()
   let statesToSolution = []
   while (current != null) {
     if (current.operator != null) {
@@ -74,20 +44,10 @@ const uniformCost = (startState, goalState) => {
   console.log(`Deepest level expanded: ${deepestExpanded}\n`)
 
   console.log(`Path size: ${path.length} \n Path: ${path} \n`)
-  // for (const state of statesToSolution) {
-  //   displayBoard(state)
-  // }
+  for (const state of statesToSolution) {
+    displayBoard(state)
+  }
 }
-
-console.log('Uniform Cost \n')
-console.time()
-uniformCost([4, 1, 3, 7, 2, 5, 8, ' ', 6], goalState)
-console.timeEnd()
-console.time()
-uniformCost([1, ' ', 2, 5, 7, 3, 4, 8, 6], goalState)
-console.timeEnd()
-console.log('--------------------------------------\n')
-// uniformCost([7, 4, 8, ' ', 5, 2, 1, 3, 6], goalState)
 
 const getHeuristic = (state, goal) => {
   let distanceToMatch = 0
@@ -100,6 +60,8 @@ const getHeuristic = (state, goal) => {
 }
 
 const aStar = (startState, goalState) => {
+  console.log('----------- Running A* -----------')
+  console.time()
   const startNode = createNode(startState, null, null, 0, 0)
   let fringe = []
   let visited = []
@@ -123,7 +85,7 @@ const aStar = (startState, goalState) => {
     visited.push(current)
     current = fringe.shift()
   }
-
+  console.timeEnd()
   let statesToSolution = []
   while (current != null) {
     if (current.operator != null) {
@@ -139,18 +101,11 @@ const aStar = (startState, goalState) => {
   console.log(`Deepest level expanded: ${deepestExpanded}\n`)
 
   console.log(`Path size: ${path.length} \n Path: ${path} \n`)
-  // for (const state of statesToSolution) {
-  //   displayBoard(state)
-  // }
+  for (const state of statesToSolution) {
+    displayBoard(state)
+  }
 }
 
-console.log('A * \n')
-console.time()
-aStar([4, 1, 3, 7, 2, 5, 8, ' ', 6], goalState)
-console.timeEnd()
-console.time()
-aStar([1, ' ', 2, 5, 7, 3, 4, 8, 6], goalState)
-console.timeEnd()
-console.log('--------------------------------------\n')
-
-// aStar([7, 4, 8, ' ', 5, 2, 1, 3, 6], goalState)
+let board = generateNewBoard()
+uniformCost(board, goalState)
+aStar(board, goalState)
